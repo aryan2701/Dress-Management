7ac5363fdc72e81bf49966d64b64366a3b64279d
aryan2701 <101573545+aryan2701@users.noreply.github.com>
2024-12-09 01:28:16 +0530

    Update main.jsx

--- FRONTEND/dress-management-frontend/src/main.jsx
+++ FRONTEND/dress-management-frontend/src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/risingsun">
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
-        rel="stylesheet"
-      />
-      <App />
-    </BrowserRouter>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <App />
   </StrictMode>
 );
