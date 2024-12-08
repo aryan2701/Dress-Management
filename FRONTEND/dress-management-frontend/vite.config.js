e46c8242b4b82f3bf80f43ebdffc6284c601ab60
aryan2701 <101573545+aryan2701@users.noreply.github.com>
2024-12-09 01:08:19 +0530

    Update vite.config.js

--- FRONTEND/dress-management-frontend/vite.config.js
+++ FRONTEND/dress-management-frontend/vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/risingsun/',  // Add this line for deployment under /risingsun
-});
-
+})
